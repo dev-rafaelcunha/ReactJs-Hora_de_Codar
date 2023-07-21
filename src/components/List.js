a90c9aa7ae1be47176f464145a63e169ae425546
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-07-21 00:06:07 -0300

    Utilizando PropTypes.

--- src/components/List.js
+++ src/components/List.js
@@ -7,9 +7,11 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca='Ferrari' />
-                <Item marca='Fiat' />
+                <Item marca='Ferrari' ano_lancamento={1985} />
+                <Item marca='Fiat' ano_lancamento={1964} />
                 <Item marca='Reanult' />
+                <Item marca='Chevrolet' ano_lancamento={1999} />
+                <Item />
             </ul>
         </>
     );
