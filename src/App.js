438bddfa3e76531a1688571c18a37081e49aeeca
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-07-27 18:18:44 -0300

    Implementando o React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import './App.css';
-import { useState } from "react"
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Home from './pages/Home';
 
 function App() {
 
-  const [nome, setNome] = useState();
-
   return (
-    <div className='App'>
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome} />
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/empresa" element={<Empresa />}></Route>
+          <Route exact path="/contato" element={<Contato />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
   );
 }
 
-export default App;
+export default App
---
+++ src/pages/Empresa.js
@@ -0,0 +1,12 @@
+function Empresa() {
+
+    return (
+
+        <div>
+            <h1>Empresa</h1>
+            <p>Conteúdo da Página</p>
+        </div>
+    )
+}
+
+export default Empresa
