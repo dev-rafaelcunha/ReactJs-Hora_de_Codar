bba383745715d3ba9a6ea7dbba0894faee7dd35c
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-07-27 23:46:38 -0300

    Icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
-import Contato from './pages/Contato';
-import Empresa from './pages/Empresa';
-import Home from './pages/Home';
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/empresa" element={<Empresa />}></Route>
-          <Route exact path="/contato" element={<Contato />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route path='/empresa' element={<Empresa />}></Route>
+        <Route path='/contato' element={<Contato />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
